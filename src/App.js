c1a5a20c1b276c04c6e2fc9bc528c7e49af716b8
Emil Schultz <emilrschultz@gmail.com>
2021-01-29 11:48:42 +0100

    branch test

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import React from 'react';
 function App() {
   return (
     <>
-      <h1>Hallo verden!</h1>
-      <p>
-        Jeg er en veldig enkel React-app som du kan endre og leke med som du vil.
-      </p>
+      <p>Branch test</p>
     </>
   )
 };
