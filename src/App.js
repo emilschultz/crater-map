7962fcb9e1a05e63e9414bded1e62c87402885b6
Emil Schultz <emilrschultz@gmail.com>
2021-02-01 11:57:17 +0100

    React app linked to Comsic project

--- src/App.js
+++ src/App.js
@@ -1,9 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Cosmic from 'cosmicjs';
 
 function App() {
+
+  const [pageData, setPageData] = useState(null);
+
+    useEffect(() => {
+      const client = new Cosmic()
+      const bucket = client.bucket({
+        slug: process.env.BUCKET_SLUG,
+        read_key: process.env.READ_KEY
+      });
+
+      bucket.getObject({
+        slug: 'crater-map',
+        props: 'slug,title,content'
+      })
+      .then(data => {
+        setPageData(data.object);
+      })
+      .catch(error => {
+        console.log(error)
+      })
+    }, [])
+
+  const renderSkeleton = () => {
+    return(
+      <p>Loading...</p>
+    )
+  }
+  
+  const renderPage = () => {
+    return(  
+      <main>
+        <h1>{pageData.title}</h1>
+        <div dangerouslySetInnerHTML={{__html: pageData.content}} />
+      </main>
+    ) 
+  }
+
+
   return (
     <>
-      <p>Branch test2</p>
+      {(pageData === null) ? renderSkeleton() : renderPage()}
     </>
   )
 };
