6bc84bca732f5cc901ae945d804b1d338a1472e5
Emil Schultz <emilrschultz@gmail.com>
2021-02-02 12:25:20 +0100

    Set up plotly

---
+++ src/components/SkeletonComponent/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const SkeletonComponent = () => {
+  return(
+      <div style={{
+        height: '800px',
+        backgroundColor: '#e5e5e5',
+        marginTop: '115px',
+        }}>
+      </div>
+  )
+}
+
+export default SkeletonComponent;
