fd49084f24ea03ef9dee53f3ad4c9a9050a08231
Emil Schultz <emilrschultz@gmail.com>
2021-02-02 14:11:19 +0100

    added controls and fullscreen option for map. Added markers for five different impact craters

--- src/App.js
+++ src/App.js
@@ -1,31 +1,15 @@
 import React, { useState, useEffect, useRef } from 'react';
 import Cosmic from 'cosmicjs';
 import Mapbox from 'mapbox-gl';
-import Plot from 'react-plotly.js';
-
-// COMPONENTS
+// COMPONENTS IMPORT
 import SkeletonComponent from './components/SkeletonComponent'
+import Chart from './components/Chart';
+
 
 
 function App() {
 
   const [pageData, setPageData] = useState(null);
-  const [chartState, setChartState] = useState({
-    data: [{
-      x: [1, 2, 3],
-      y: [2, 6, 4],
-      type: 'scatter',
-      mode: 'lines+markers',
-      marker: {color: 'blue'}
-    }],
-    layout: {
-      width: 800,
-      height: 400,
-      title: 'Diagram'
-    }, 
-    frames: [],
-    config: {}
-  })
 
   let map = null;
   const mapElement = useRef(null)
@@ -55,10 +39,70 @@ function App() {
     useEffect(() => {
       map = new Mapbox.Map({
         container: mapElement.current,
-        style: 'mapbox://styles/mapbox/light-v10',
-        center: [12.567707560916299, 55.68859077933342],
-        zoom: 13
+        style: 'mapbox://styles/mapbox-map-design/ckhqrf2tz0dt119ny6azh975y',
+        center: [10.13980512713666, 53.39893116868396],
+        zoom: 2,
+        pitch: 45,
       })
+      // CONTROLS (FULLSCREEN & NAVIGATION)
+      map.addControl(new Mapbox.FullscreenControl());
+      map.addControl(new Mapbox.NavigationControl());
+
+      // CRATER MARKERS
+
+      // BERRINGER CRATER
+      const berringerMarker = new Mapbox.Marker(event)
+      berringerMarker.setLngLat([-111.02226922824839, 35.028046414736714])
+      berringerMarker.addTo(map)
+
+      // WOLF CREEK CRATER
+      const wolfCreekMarker = new Mapbox.Marker(event)
+      wolfCreekMarker.setLngLat([127.7955367736068, -19.171854725200237])
+      wolfCreekMarker.addTo(map)
+
+      // AMGUID CRATER
+      const amguidMarker = new Mapbox.Marker(event)
+      amguidMarker.setLngLat([4.395014134448849, 26.087573417980202])
+      amguidMarker.addTo(map)
+
+      // PINGUALUIT CRATER
+      const pingualuiMarker = new Mapbox.Marker(event)
+      pingualuiMarker.setLngLat([-73.65996457312208, 61.27817154641245])
+      pingualuiMarker.addTo(map)
+
+      // KAALI CRATER
+      const kaaliMarker = new Mapbox.Marker(event)
+      kaaliMarker.setLngLat([22.669319690218423, 58.372753194660284])
+      kaaliMarker.addTo(map)
+
+
+
+
+
+      // 3D LAYER
+      map.on('load', function () {
+        map.addSource('mapbox-dem', {
+        'type': 'raster-dem',
+        'url': 'mapbox://mapbox.mapbox-terrain-dem-v1',
+        'tileSize': 512,
+        'maxzoom': 14
+        });
+        // add the DEM source as a terrain layer with exaggerated height
+        map.setTerrain({ 'source': 'mapbox-dem', 'exaggeration': 1.5 });
+         
+        // add a sky layer that will show when the map is highly pitched
+        map.addLayer({
+        'id': 'sky',
+        'type': 'sky',
+        'paint': {
+        'sky-type': 'atmosphere',
+        'sky-atmosphere-sun': [0.0, 0.0],
+        'sky-atmosphere-sun-intensity': 15
+        }
+        });
+        });
+
+        
     }, [pageData])
 
     
@@ -74,25 +118,17 @@ function App() {
       <main>
         <h1>{pageData.title}</h1>
         <div dangerouslySetInnerHTML={{__html: pageData.content}} />
-        <Plot
-          data={chartState.data}
-          layout={chartState.layout}
-          frames={chartState.frames}
-          config={chartState.config}
-          onInitialized={(figure) => setChartState(figure)}
-          onUpdate={(figure) => setChartState(figure)}
-        />
       </main>
     ) 
   }
-
 
   return (
     <>
       {(pageData === null) ? renderSkeleton() : renderPage()}
       <div style={{height: '84vh'}} ref={mapElement}></div>
+      <Chart />
     </>
   )
-};
+}
 
 export default App;
