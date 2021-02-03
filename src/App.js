f2971d668b9437eb002c3d6af063ace8dec423f3
Emil Schultz <emilrschultz@gmail.com>
2021-02-03 15:37:34 +0100

    flyTo functions added and 3D layering adjustet

--- src/App.js
+++ src/App.js
@@ -5,7 +5,12 @@ import Mapbox from 'mapbox-gl';
 import SkeletonComponent from './components/SkeletonComponent'
 import Chart from './components/Chart';
 
+// export const GeoJson = {
+//   'type': 'FeatureCollection',
+//   'features': [
 
+//   ]
+// }
 
 function App() {
 
@@ -39,11 +44,12 @@ function App() {
     useEffect(() => {
       map = new Mapbox.Map({
         container: mapElement.current,
-        style: 'mapbox://styles/mapbox-map-design/ckhqrf2tz0dt119ny6azh975y',
+        style: 'mapbox://styles/mapbox/satellite-v9',
         center: [10.13980512713666, 53.39893116868396],
-        zoom: 2,
+        zoom: 3,
         pitch: 45,
       })
+      
       // CONTROLS (FULLSCREEN & NAVIGATION)
       map.addControl(new Mapbox.FullscreenControl());
       map.addControl(new Mapbox.NavigationControl());
@@ -74,21 +80,21 @@ function App() {
       const kaaliMarker = new Mapbox.Marker(event)
       kaaliMarker.setLngLat([22.669319690218423, 58.372753194660284])
       kaaliMarker.addTo(map)
+    
 
 
-
-
+      
 
       // 3D LAYER
       map.on('load', function () {
         map.addSource('mapbox-dem', {
         'type': 'raster-dem',
         'url': 'mapbox://mapbox.mapbox-terrain-dem-v1',
-        'tileSize': 512,
+        'tileSize': 1024,
         'maxzoom': 14
         });
         // add the DEM source as a terrain layer with exaggerated height
-        map.setTerrain({ 'source': 'mapbox-dem', 'exaggeration': 1.5 });
+        map.setTerrain({ 'source': 'mapbox-dem', 'exaggeration': 2.2 });
          
         // add a sky layer that will show when the map is highly pitched
         map.addLayer({
@@ -105,7 +111,79 @@ function App() {
         
     }, [pageData])
 
-    
+// --------------------------------------------------------------------------------
+
+const customMarker = () => {
+  let el = document.createElement('div');
+  el.style.display = 'block';
+  el.style.width = '40px';
+  el.style.height = '40px';
+  el.style.backgroundImage = 'url("https://images.unsplash.com/photo-1611602132416-da2045990f76?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=634&q=80")';
+  el.style.backgroundSize = '40px 40px';
+}
+
+
+
+
+
+
+
+
+
+
+    // FLYTO CRATERS
+
+  const flyToBerringer = () => {
+    map.flyTo({
+      center: [-111.02226922824839, 35.028046414736714],
+      zoom: 15,
+      speed: 0.6,
+      curve: 1.5,
+      essential: true
+    })
+  }
+
+  const flyToWolfCreek = () => {
+    map.flyTo({
+      center: [127.7955367736068, -19.171854725200237],
+      zoom: 15,
+      speed: 0.6,
+      curve: 1.5,
+      essential: true
+    })
+  }
+
+  const flyToAmguid = () => {
+    map.flyTo({
+      center: [4.395014134448849, 26.087573417980202],
+      zoom: 16,
+      speed: 0.6,
+      curve: 1.5,
+      essential: true
+    })
+  }
+
+  const flyToPingualuit = () => {
+    map.flyTo({
+      center: [-73.65996457312208, 61.27817154641245],
+      zoom: 13,
+      speed: 0.6,
+      curve: 1.5,
+      essential: true 
+    })
+  }
+
+  const flyToKaali = () => {
+    map.flyTo({
+      center: [22.669319690218423, 58.372753194660284],
+      zoom: 16,
+      speed: 0.6,
+      curve: 1.5,
+      essential: true
+    })
+  }
+
+// --------------------------------------------------------------------------------
 
   const renderSkeleton = () => {
     return(
@@ -118,6 +196,14 @@ function App() {
       <main>
         <h1>{pageData.title}</h1>
         <div dangerouslySetInnerHTML={{__html: pageData.content}} />
+        <button onClick={flyToBerringer}>Berringer Crater</button>
+        <button onClick={flyToWolfCreek}>Wolf Creek Crater</button>
+        <button onClick={flyToAmguid}>Amguid Crater</button>
+        <button onClick={flyToPingualuit}>Pingualuit Crater</button>
+        <button onClick={flyToKaali}>Kaali Crater</button>
+
+
+        
       </main>
     ) 
   }
