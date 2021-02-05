683065b4e19d38fe19f362b984ed6bbbcc2a3577
Emil Schultz <emilrschultz@gmail.com>
2021-02-05 10:22:49 +0100

    Fonts and colors

--- src/components/TitleSection/index.jsx
+++ src/components/TitleSection/index.jsx
@@ -8,21 +8,30 @@ const TitleSection = styled.section`
   align-items: center;
   justify-content: center;
   text-align: center;
-  background-color: #000000;
-  color: #EDEBE3;
+  background-color: #193620;
+  color: #f2361c;
+  font-family: 'Rakkas', cursive;
 
   h1 {
-    font-size: 2rem;
+    font-size: 6rem;
+    margin: 0;
   }
 
   div {
     width:50%;
     display: flex;
     justify-content: center;
+    text-align: justify;
   }
 
+  p {
+    font-size: 1.4rem;
+    }
+
   a {
-    font-size: 2rem
+    font-size: 2.5rem;
+    cursor: pointer;
+    padding: 1rem;
   }
 `;
 
