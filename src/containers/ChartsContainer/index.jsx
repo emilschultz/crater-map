072b67e2cd26e6c5242303b4380eda78be6bc987
emilschultz <emil.schultz@amedia.no>
2023-02-19 21:02:21 +0100

    fix build // linting

--- src/containers/ChartsContainer/index.jsx
+++ src/containers/ChartsContainer/index.jsx
@@ -1,198 +1,252 @@
-import React, { useState, useEffect } from 'react';
-import Plot from 'react-plotly.js';
+import React, { useState, useEffect } from "react";
+import Plot from "react-plotly.js";
 
-import ChartSection from '../../components/ChartSection';
+import ChartSection from "../../components/ChartSection";
 
 const nasaApiKey = process.env.NASA_API_KEY;
 
-function Asteroids () {
-
+function Asteroids() {
   // BAR CHART - SOME KNOWN ASTEROIDS AND THEIR DIAMETER IN KILOMETERS
-  const [ barChartState, setBarChartState ] = useState({
+  const [barChartState, setBarChartState] = useState({
     data: [],
     layout: {
       width: 920,
       height: 640,
-      plot_bgcolor:'rgb(0, 0, 0)',
-      paper_bgcolor:'rgb(0, 0, 0)',
+      plot_bgcolor: "rgb(0, 0, 0)",
+      paper_bgcolor: "rgb(0, 0, 0)",
       title: {
-        text: 'Known Asteroids and their diameter in kilometers',
+        text: "Known Asteroids and their diameter in kilometers",
         font: {
-          family: 'Arial',
+          family: "Arial",
           size: 24,
-          color: 'rgb(200, 100, 40)'
-        }
+          color: "rgb(200, 100, 40)",
+        },
       },
       xaxis: {
         title: {
-          text: 'ASTEROID NAME',
+          text: "ASTEROID NAME",
           font: {
-            family: 'ARIAL',
+            family: "ARIAL",
             size: 14,
-            color: 'rgb(200, 100, 40)'
-          }
+            color: "rgb(200, 100, 40)",
+          },
         },
         color: "rgb(200, 100, 40)",
       },
       yaxis: {
         title: {
-          text: 'DIAMETER IN KILOMETERS',
+          text: "DIAMETER IN KILOMETERS",
           font: {
-            family: 'ARIAL',
+            family: "ARIAL",
             size: 14,
-          }
+          },
         },
-        color: 'rgb(200, 100, 40)'
-
-      }
+        color: "rgb(200, 100, 40)",
+      },
     },
     frames: [],
-    config: {}
-  })
+    config: {},
+  });
 
   useEffect(() => {
     let newBarChartData = {
       x: [],
       y: [],
-      type: 'bar',
+      type: "bar",
       marker: {
-        color: ['#FF8E00', '#E54818', '#FF7F00', '#FF551B', '#FF8E00', '#FF6702', '#FF8E00', '#E54818', '#FF7F00', '#FF551B', '#FF8E00', '#FF6702', '#FF8E00', '#E54818', '#FF7F00', '#FF551B', '#FF8E00', '#FF6702', '#FF8E00', '#E54818', '#FF7F00', '#FF551B', '#FF8E00', '#FF6702'],
-      }
+        color: [
+          "#FF8E00",
+          "#E54818",
+          "#FF7F00",
+          "#FF551B",
+          "#FF8E00",
+          "#FF6702",
+          "#FF8E00",
+          "#E54818",
+          "#FF7F00",
+          "#FF551B",
+          "#FF8E00",
+          "#FF6702",
+          "#FF8E00",
+          "#E54818",
+          "#FF7F00",
+          "#FF551B",
+          "#FF8E00",
+          "#FF6702",
+          "#FF8E00",
+          "#E54818",
+          "#FF7F00",
+          "#FF551B",
+          "#FF8E00",
+          "#FF6702",
+        ],
+      },
     };
     // Browse the overall Asteroid data-set
     fetch(`https://api.nasa.gov/neo/rest/v1/neo/browse?api_key=${nasaApiKey}`)
-    .then(response => response.json())
-    .then(data => {
-      data.near_earth_objects.forEach(object => {
-        // console.log(object)
-        newBarChartData.x.push(object.name_limited)
-        newBarChartData.y.push(object.estimated_diameter.kilometers.estimated_diameter_max)
-        
+      .then((response) => response.json())
+      .then((data) => {
+        data.near_earth_objects.forEach((object) => {
+          // console.log(object)
+          newBarChartData.x.push(object.name_limited);
+          newBarChartData.y.push(
+            object.estimated_diameter.kilometers.estimated_diameter_max
+          );
+        });
+        let newBarChartState = {
+          ...barChartState,
+          data: [newBarChartData],
+        };
+
+        setBarChartState(newBarChartState);
+      })
+      .catch((error) => {
+        console.log(error);
       });
-      let newBarChartState = {
-        ...barChartState,
-        data: [newBarChartData],
-      }
-  
-      setBarChartState(newBarChartState);
-    })
-    .catch(error => {
-      console.log(error)
-    })
-    }, [])
+  }, []);
 
-    // -------------------------------------------------------------------
+  // -------------------------------------------------------------------
 
-    // BUBBLE CHART ASTEROIDS NEAREST EARTH TODAY
-    const [ bubbleChartState, setBubbleChartState ] = useState({
-      data: [],
-      layout: {
-        width: 920,
-        height: 920,
-        plot_bgcolor:'rgb(0, 0, 0)',
-        paper_bgcolor:'rgb(0, 0, 0)',
-        
-        title: {
-          text: 'Asteroids nearest earth today',
-          font: {
-            family: 'Arial',
-            size: 24,
-            color: 'rgb(200, 100, 40)'
-          }
+  // BUBBLE CHART ASTEROIDS NEAREST EARTH TODAY
+  const [bubbleChartState, setBubbleChartState] = useState({
+    data: [],
+    layout: {
+      width: 920,
+      height: 920,
+      plot_bgcolor: "rgb(0, 0, 0)",
+      paper_bgcolor: "rgb(0, 0, 0)",
+
+      title: {
+        text: "Asteroids nearest earth today",
+        font: {
+          family: "Arial",
+          size: 24,
+          color: "rgb(200, 100, 40)",
         },
-        xaxis: {
-          title: {
-            text: 'TIMESTAMP',
-            font: {
-              family: 'Arial',
-              size: 14,
-              color: 'rgb(200, 100, 40)'
-            }
-          },
-          color: 'rgb(200, 100, 40)',
-        },
-        yaxis: {
-          title: {
-            text: 'MISS DISTANCE IN KILOMETERS',
-            font: {
-              family: 'Arial',
-              size: 14,
-            }
-          },
-          color: 'rgb(200, 100, 40)'
-        }
       },
-      frames: [],
-      config: {}
-    });
+      xaxis: {
+        title: {
+          text: "TIMESTAMP",
+          font: {
+            family: "Arial",
+            size: 14,
+            color: "rgb(200, 100, 40)",
+          },
+        },
+        color: "rgb(200, 100, 40)",
+      },
+      yaxis: {
+        title: {
+          text: "MISS DISTANCE IN KILOMETERS",
+          font: {
+            family: "Arial",
+            size: 14,
+          },
+        },
+        color: "rgb(200, 100, 40)",
+      },
+    },
+    frames: [],
+    config: {},
+  });
 
-    useEffect(() => {
-      let newBubbleChartData = {
-        x: [],
-        y: [],
-        text: [],
-        mode: 'markers',
-        marker: {
-          size: [],
-          color: ['#FF8E00', '#E54818', '#FF7F00', '#FF551B', '#FF8E00', '#FF6702', '#FF8E00', '#E54818', '#FF7F00', '#FF551B', '#FF8E00', '#FF6702', '#FF8E00', '#E54818', '#FF7F00', '#FF551B', '#FF8E00', '#FF6702']
+  useEffect(() => {
+    let newBubbleChartData = {
+      x: [],
+      y: [],
+      text: [],
+      mode: "markers",
+      marker: {
+        size: [],
+        color: [
+          "#FF8E00",
+          "#E54818",
+          "#FF7F00",
+          "#FF551B",
+          "#FF8E00",
+          "#FF6702",
+          "#FF8E00",
+          "#E54818",
+          "#FF7F00",
+          "#FF551B",
+          "#FF8E00",
+          "#FF6702",
+          "#FF8E00",
+          "#E54818",
+          "#FF7F00",
+          "#FF551B",
+          "#FF8E00",
+          "#FF6702",
+        ],
+      },
+    };
 
-        }
-      };
+    // GET TODAY'S DATE
+    let today = new Date();
+    let yyyy = String(today.getFullYear());
+    let mm = String(today.getMonth() + 1);
+    let dd = String(today.getDate());
 
-      // GET TODAY'S DATE
-      let today = new Date();
-      let yyyy = String(today.getFullYear());
-      let mm = String(today.getMonth() + 1);
-      let dd = String(today.getDate());
+    today = yyyy + "-" + mm + "-" + dd;
 
-      today = yyyy + '-' + mm + '-' + dd;
-
-      fetch('https://api.nasa.gov/neo/rest/v1/feed?start_date=' + today + '&end_date=' + today + '&api_key=7fX5ch29dBRLQKQVxaI7twG9p1ZVmeNY1dSVaSsg')
-      .then(response => response.json())
-      .then(data => {
-        const values = Object.values(data.near_earth_objects)
-        values[0].forEach(object => {
-          newBubbleChartData.x.push(object.close_approach_data.[0].close_approach_date_full)
-          newBubbleChartData.y.push(object.close_approach_data.[0].miss_distance.kilometers)
-          newBubbleChartData.marker.size.push(object.estimated_diameter.meters.estimated_diameter_min * 0.5)
-          newBubbleChartData.text.push(object.close_approach_data.[0].close_approach_date_full)
+    fetch(
+      "https://api.nasa.gov/neo/rest/v1/feed?start_date=" +
+        today +
+        "&end_date=" +
+        today +
+        "&api_key=7fX5ch29dBRLQKQVxaI7twG9p1ZVmeNY1dSVaSsg"
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const values = Object.values(data.near_earth_objects);
+        values[0].forEach((object) => {
+          newBubbleChartData.x.push(
+            object.close_approach_data.close_approach_date_full
+          );
+          newBubbleChartData.y.push(
+            object.close_approach_data.miss_distance.kilometers
+          );
+          newBubbleChartData.marker.size.push(
+            object.estimated_diameter.meters.estimated_diameter_min * 0.5
+          );
+          newBubbleChartData.text.push(
+            object.close_approach_data.close_approach_date_full
+          );
         });
 
         let newBubbleChartState = {
           ...bubbleChartState,
-          data: [newBubbleChartData]
+          data: [newBubbleChartData],
         };
 
         setBubbleChartState(newBubbleChartState);
       })
-      .catch(error => {
-        console.log(error)
-      })
-    }, [])
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
-    return(
-      <ChartSection>
-
-        <Plot
+  return (
+    <ChartSection>
+      <Plot
         data={barChartState.data}
         layout={barChartState.layout}
         frames={barChartState.frames}
         config={barChartState.config}
         onInitialized={(figure) => setBarChartState(figure)}
         onUpdate={(figure) => setBarChartState(figure)}
-        />
+      />
 
-        <Plot
+      <Plot
         data={bubbleChartState.data}
         layout={bubbleChartState.layout}
         frames={bubbleChartState.frames}
         config={bubbleChartState.config}
         onInitialized={(figure) => setbubbleChartState(figure)}
         onUpdate={(figure) => setbubbleChartState(figure)}
-        />
-
-      </ChartSection>
-    )
+      />
+    </ChartSection>
+  );
 }
 
 export default Asteroids;
