a33e1aa2abf0b91b637f97c1914c9d16be623197
emilschultz <emil.schultz@amedia.no>
2023-02-19 20:37:07 +0100

    update webpack config

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,43 +1,41 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const webpack = require('webpack');
-const Dotenv = require('dotenv-webpack');
-const CopyPlugin = require('copy-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack = require("webpack");
+const Dotenv = require("dotenv-webpack");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = () => {
   return {
-    mode: 'development',
+    mode: "development",
     entry: {
-      index: './src/index.js'
+      index: "./src/index.js",
     },
-    devtool: 'inline-source-map',
+    devtool: "inline-source-map",
     devServer: {
-      static: './dist',
+      static: "./dist",
       hot: true,
-      historyApiFallback: true
+      historyApiFallback: true,
     },
     plugins: [
       new Dotenv({
-        path: './.development.env'
+        path: "./.development.env",
       }),
       new CleanWebpackPlugin(),
       new HtmlWebpackPlugin({
-        filename: 'index.html',
-        template: './src/template.html',
-        title: 'Development'
+        filename: "index.html",
+        template: "./src/index.html",
+        title: "Development",
       }),
       new CopyPlugin({
-        patterns: [
-          {from: './other', noErrorOnMissing: true}
-        ],
+        patterns: [{ from: "./other", noErrorOnMissing: true }],
       }),
-      new webpack.HotModuleReplacementPlugin()
+      new webpack.HotModuleReplacementPlugin(),
     ],
     output: {
-      filename: '[name].bundle.js',
-      path: path.resolve(__dirname, 'dist'),
-      publicPath: '/'
+      filename: "[name].bundle.js",
+      path: path.resolve(__dirname, "dist"),
+      publicPath: "/",
     },
     module: {
       rules: [
@@ -45,20 +43,20 @@ module.exports = () => {
           test: /\.(js|jsx)$/,
           exclude: /node_modules/,
           use: {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
-              plugins: ['@babel/plugin-transform-runtime']
-            }
-          }
+              plugins: ["@babel/plugin-transform-runtime"],
+            },
+          },
         },
         {
           test: /\.(png|svg|jpg|jpeg|gif)$/i,
-          type: 'asset/resource',
-        }
+          type: "asset/resource",
+        },
       ],
     },
     resolve: {
-      extensions: ['*', '.js', '.jsx']
-    }
+      extensions: ["*", ".js", ".jsx"],
+    },
   };
 };
